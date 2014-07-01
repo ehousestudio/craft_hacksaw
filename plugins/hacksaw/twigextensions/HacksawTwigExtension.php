03f9f9e4c0d1fbae18ad47dcdc2d306713faf70c
Ryan Shrum <ryan@ehousestudio.com>
2014-07-01 16:37:49 -0400

    added close tags function to force close of open html elements
    Signed-off-by: Ryan Shrum <ryan@ehousestudio.com>

--- plugins/hacksaw/twigextensions/HacksawTwigExtension.php
+++ plugins/hacksaw/twigextensions/HacksawTwigExtension.php
@@ -53,7 +53,7 @@ class HacksawTwigExtension extends Twig_Extension
 		}
 
 		// Return the new content
-		return $new_content;
+		return $this->_close_tags($new_content);
 	}
 
 	// Helper Function - Truncate by Word Limit
@@ -101,6 +101,44 @@ class HacksawTwigExtension extends Twig_Extension
 		
 		return $content;
 
-	}	
+	}
+	
+	function _close_tags($content) {
+		
+		preg_match_all('#<(?!meta|img|br|hr|input\b)\b([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $content, $result);
+		
+		$openedtags = $result[1];
+		
+		preg_match_all('#</([a-z]+)>#iU', $content, $result);
+		
+		$closedtags = $result[1];
+		
+		$len_opened = count($openedtags);
+		
+		if (count($closedtags) == $len_opened) {
+
+			return $content;
+
+		}
+
+		$openedtags = array_reverse($openedtags);
+
+		for ($i=0; $i < $len_opened; $i++) {
+
+			if (!in_array($openedtags[$i], $closedtags)) {
+
+				$content .= '</'.$openedtags[$i].'>';
+
+			} else {
+
+				unset($closedtags[array_search($openedtags[$i], $closedtags)]);
+
+			}
+
+		}
+
+		return $content;
+		
+	} 
 
 }
